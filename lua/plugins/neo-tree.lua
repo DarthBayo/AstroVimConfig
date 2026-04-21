8ffd8254768e53f84592573c484ede9e16a99091
Giovani Quisini <giovaniequisini@gmail.com>
2026-04-21 20:44:00 -0300

    VRAAAAAUm

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,6 +9,25 @@ return {
     window = {
       position = "left",
       width = 50,
+      mappings = {
+        ["gy"] = {
+          function(state)
+            local node = state.tree:get_node()
+            local rel = vim.fn.fnamemodify(node.path, ":~:.")
+            vim.fn.setreg("+", rel)
+            vim.notify("Copied: " .. rel, vim.log.levels.INFO)
+          end,
+          desc = "Copy relative path",
+        },
+        ["gY"] = {
+          function(state)
+            local node = state.tree:get_node()
+            vim.fn.setreg("+", node.path)
+            vim.notify("Copied: " .. node.path, vim.log.levels.INFO)
+          end,
+          desc = "Copy absolute path",
+        },
+      },
     },
     filesystem = {
       filtered_items = {
